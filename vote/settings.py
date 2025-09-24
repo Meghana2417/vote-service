928fc5745bcc976e07a383503fdf0405edaf90c2
Meghana S <meghana.s.18025@gmail.com>
2025-09-24 16:45:27 +0530

    updated file

--- vote/settings.py
+++ vote/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'vote.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'vote',
-        'CLIENT' : {
-            'host': 'mongodb',
-            'port': 27017,
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
